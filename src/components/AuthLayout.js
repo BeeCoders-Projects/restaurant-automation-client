d0cab0977c9b88a4de46a3842f733fa676db6364
Daniil M <41665964+DANLEP@users.noreply.github.com>
2023-05-21 12:19:52 +0300

    header fix

--- src/components/AuthLayout.js
+++ src/components/AuthLayout.js
@@ -6,7 +6,7 @@ function AuthLayout(){
         <>
             <header className="h-32 bg-[#2D2929] flex justify-center items-center relative">
                 <Logo className="absolute max-w-24 left-4"/>
-                <h1 className="text-5xl text-white">Bee Cafe</h1>
+                <h1 className="text-5xl text-white my-8">Bee Cafe</h1>
             </header>
             <main className="flex items-center justify-center grow py-12 font-light auth">
                 <div className="text-4xl w-[1024px] h-[672px]
