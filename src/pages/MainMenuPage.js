2b2a625d61badb893a362f6f1a18a94e54d87316
Daniil M <41665964+DANLEP@users.noreply.github.com>
2023-05-26 15:11:01 +0300

    Added grid menu

--- src/pages/MainMenuPage.js
+++ src/pages/MainMenuPage.js
@@ -6,7 +6,7 @@ function MainMenuPage(){
     return(
         <>
             {/*тут потом категории будем добавлять, боковое меню, хэдер 2 уровня*/}
-            <div className="pt-9 grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+            <div className="h-full pt-9 grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
                 <DishItemList/>
             </div>
         </>
