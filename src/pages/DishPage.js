fd22cec2d8dc236b5755ea8a5d80bf198170789f
Nastya <anastasiia.novoselova@nure.ua>
2023-05-27 16:42:38 +0300

    DONE: RAS-93-Implement_Dish_Page_layout 2

--- src/pages/DishPage.js
+++ src/pages/DishPage.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Button from "../components/Button"
-// import {ReactComponent as Img} from "../img/filadelfiya.jpeg";
+import React from "react";
 function DishPage () {
     const icon = "https://ras-demo-bucket.s3.amazonaws.com/dishes/1.jpg"
     const dynamicBgStyle = {
@@ -10,25 +10,30 @@ function DishPage () {
         <>
             <div className="grid h-full">
                 <Header/>
-                <main className="overflow-auto text-4xl w-full flex pt-9 px-12">
+                <main className="overflow-auto w-full flex pl-18 pr-48 pt-20">
                     <div
                         className="w-[478px] h-[313px] overflow-hidden rounded-xl mb-5 bg-cover bg-center mr-8"
                         style={dynamicBgStyle}
                     >
                     </div>
-                    {/*<div>*/}
-                    {/*    <img width="478" height="313" src="" alt="img"/>*/}
-                    {/*    /!*<Img className="absolute max-w-119 max-h-78"/>*!/*/}
-                    {/*</div>*/}
                     <div>
-                        <h2>Сет каліфорнія</h2>
-                        <p>Копчений лосось з додаванням вугрю та крем-сиру. Подаємо на молочній булочці з ікрою тобіко та прикрашаємо нитками чилі.</p>
-                        <p>Копчений лосось, вугрь, крем-сир, молочна булочка, ікра тобіко, нитки чилі</p>
-                        <div>
+                        <p className="text-4xl">Сет каліфорнія</p>
+                        <p className="text-xl pt-4">Копчений лосось з додаванням вугрю та крем-сиру. Подаємо на молочній булочці з ікрою тобіко та прикрашаємо нитками чилі.</p>
+                        <p className="text-xl pt-4">Копчений лосось, вугрь, крем-сир, молочна булочка, ікра тобіко, нитки чилі</p>
+                        <div className="text-2xl pt-4 pl-116 flex">
                             <p>1200 грн</p>
-                            <p>400 г</p>
+                            <p className="ml-15">400 г</p>
                         </div>
-                        <Button>На головну</Button>
+                        <Button
+                            style={{
+                                backgroundColor:"#FFF200",
+                                fontSize:"26px",
+                                borderRadius:"15px",
+                                width:"198px",
+                                height:"49px",
+                            }}
+                            className="flex justify-center mt-15"
+                        >На головну</Button>
                     </div>
                 </main>
             </div>
