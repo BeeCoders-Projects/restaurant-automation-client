e61c3c5e3415ed2ba8c1e3874de1b23a0fcae4e3
Daniil M <41665964+DANLEP@users.noreply.github.com>
2023-05-27 15:43:51 +0300

    Dish page fix

--- src/pages/DishPage.js
+++ src/pages/DishPage.js
@@ -2,15 +2,24 @@ import Header from "../components/Header";
 import Button from "../components/Button"
 // import {ReactComponent as Img} from "../img/filadelfiya.jpeg";
 function DishPage () {
+    const icon = "https://ras-demo-bucket.s3.amazonaws.com/dishes/1.jpg"
+    const dynamicBgStyle = {
+        backgroundImage: `url(${icon || null})`,
+    };
     return (
         <>
-            <div className="w-full h-full">
+            <div className="grid h-full">
                 <Header/>
-                <main className="overflow-auto text-4xl w-full flex space-x-12 ml-18 mr-48 mt-29">
-                    <div>
-                        <img width="478" height="313" src="../img/filadelfiya.jpeg" alt="img"/>
-                        {/*<Img className="absolute max-w-119 max-h-78"/>*/}
+                <main className="overflow-auto text-4xl w-full flex pt-9 px-12">
+                    <div
+                        className="w-[478px] h-[313px] overflow-hidden rounded-xl mb-5 bg-cover bg-center mr-8"
+                        style={dynamicBgStyle}
+                    >
                     </div>
+                    {/*<div>*/}
+                    {/*    <img width="478" height="313" src="" alt="img"/>*/}
+                    {/*    /!*<Img className="absolute max-w-119 max-h-78"/>*!/*/}
+                    {/*</div>*/}
                     <div>
                         <h2>Сет каліфорнія</h2>
                         <p>Копчений лосось з додаванням вугрю та крем-сиру. Подаємо на молочній булочці з ікрою тобіко та прикрашаємо нитками чилі.</p>
