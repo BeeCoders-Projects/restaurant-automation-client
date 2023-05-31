dfe8d505e8b74c623515f7d5b66fd814e68daf8d
Daniil M <41665964+DANLEP@users.noreply.github.com>
2023-06-01 00:22:01 +0300

    RAS-114 Fixed some layout bugs

--- src/components/DishItem.js
+++ src/components/DishItem.js
@@ -16,7 +16,7 @@ function DishItem ({dish}) {
     return (
         <div className="flex flex-wrap">
     <div className={`w-fit text-xl border ${dish.special? "border-yellow-400" : "border-zinc-200"} relative h-fit
-        rounded-xl px-6 xl:px-12 py-12 pb-6 flex flex-col items-center overflow-hidden`}>
+        rounded-xl py-12 pb-6 flex flex-col items-center overflow-hidden`}>
 
         {dish.special? <div className="absolute py-1 px-5 text-yellow-500 top-0 left-0
         bg-yellow-300 rounded-br-xl font-bold font-montserrat text-base">
@@ -26,23 +26,23 @@ function DishItem ({dish}) {
             <AiOutlineInfoCircle className="absolute right-5 top-5 hover:text-yellow-400" size={30}/>
         </Link>
         <div
-            className="w-[300px] h-[125px] overflow-hidden rounded-xl mb-5 bg-cover bg-center"
+            className="w-[300px] h-[125px] overflow-hidden rounded-xl mb-5 bg-cover bg-center mx-6 xl:mx-12"
             style={dynamicBgStyle}
         >
         </div>
         <label className="w-full truncate text-center">{dish.name}</label>
-        <div className="flex mt-10 w-full justify-between items-center">
-            <div>
+        <div className="flex mt-10 w-full justify-between items-center px-6">
+            <div className="min-w-fit mx-2">
                 <p>Ціна</p>
                 <p>{dish.price} грн</p>
             </div>
-            <div>
+            <div className="min-w-fit mx-2">
                 <p>Вага</p>
                 <p>{dish.weight} г</p>
             </div>
             <Button
                 onClick={() => handleAddCart()}
-                className="flex justify-center w-24 h-fit py-2
+                className="flex justify-center w-24 h-fit py-2 min-w-fit mx-2
                 bg-yellow-300 rounded-xl text-sm hover:bg-yellow-200"
             >До кошика</Button>
         </div>
