8311c9ddf932ca997e9a783cff37e2f1d39c9b1d
Daniil M <41665964+DANLEP@users.noreply.github.com>
2023-06-01 10:09:53 +0300

    Customized scrollbar in menu and cart

--- src/components/MenuLayout.js
+++ src/components/MenuLayout.js
@@ -8,7 +8,8 @@ function MenuLayout(){
             <div className="flex h-full">
                 <div className="grid h-full flex-1" style={{"grid-template-rows": "auto 1fr"}}>
                     <Header/>
-                    <main className="overflow-auto py-9 px-12 text-4xl w-full">
+                    <main className="overflow-auto py-9 px-12 text-4xl w-full
+                    scrollbar-thumb-amber-200 scrollbar-thin scrollbar-track-gray-100">
                         <MainMenuPage/>
                     </main>
                 </div>
