9cc79e53f30d88ec3d9a6ef26622a9d5b2c4f34b
Daniil M <41665964+DANLEP@users.noreply.github.com>
2023-06-18 14:58:01 +0300

    RAS-165 Fixed card validation & finish message

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,8 @@
 import '../modal.css'
 
-export default function Modal({active, setActive, children}) {
+export default function Modal({active, setActive, closeFunc, children}) {
     return (
-        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
+        <div className={active ? "modal active" : "modal"} onClick={() => closeFunc()}>
             <div className={`py-10 px-24 ${active ? "modal__content active" : "modal__content"}`}
                  onClick={e => e.stopPropagation()}>
                 {children}
